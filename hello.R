72bb512e2d1be40843166e09b35ebeccff9f0b62
Karen Valcarce <kvalcarce@utah.gov>
2025-05-30 12:38:27 -0600

    Add hello.R with welcome message

--- hello.R
+++ hello.R
@@ -1,12 +1,12 @@
 # hello.R
 # Function to calculate incidence rate per 100,000
-calc_incidence_rate &lt;- function(cases, population) {
-  if (population &lt;= 0) stop(&quot;Population must be greater than 0&quot;)
+calc_incidence_rate <- function(cases, population) {
+  if (population <= 0) stop("Population must be greater than 0")
   (cases / population) * 100000
 }
 # Sample input
-cases &lt;- 60
-population &lt;- 10000
+cases <- 50
+population <- 120000
 # Calculate and print result
-rate &lt;- calc_incidence_rate(cases, population)
-cat(&quot;Incidence rate per 100,000 people is:&quot;, round(rate, 2), &quot;\n&quot;)
+rate <- calc_incidence_rate(cases, population)
+cat("Incidence rate per 100,000 people is:", round(rate, 2), "\n")
